a41c2dd97e8ae9242e2845b41ae77ad110d85e2f
Fomage <fomageblanc@gmail.com>
2017-11-09 13:35:06 +0100

    going to matlab

--- environment.py
+++
@@ -1,9 +0,0 @@
-
-class Environment:
-    def __init__(self, xdim, ydim):
-        self.xdim = xdim
-        self.ydim = ydim
-        self.agents = []
-
-    def add_agent(self, agent):
-        self.agents.append(agent)

e6aa70054542b5ba03848b91350c59b1b3e22df6
Fomage <fomageblanc@gmail.com>
2017-11-10 17:07:31 +0100

    Exception now thrown if no target remains

--- getAgentById.m
+++ getAgentById.m
@@ -1,6 +1,7 @@
 function agent = getAgentById(agents,id)
 %getAgentById returns the line of the agents matrix ending with id
 [agentNum,dim]=size(agents);
+agent=0;
 for i=1:agentNum
    if agents(i,dim)==id
        agent=agents(i,:);
--- updateTrackers.m
+++ updateTrackers.m
@@ -2,10 +2,18 @@ function newTrackers = updateTrackers(trackers,targets,speed)
 %updateTrackers returns the new positions of trackers
 newTrackers=trackers(:,:);
 [trackerNum,dim]=size(trackers);
+targetNum=size(targets,1);
+if targetNum <= 0
+    throw(MException('Finished:TrackersVictory','No more targets to catch'));
+end
 dim=dim-3;% ignore the team number and the current target number
 for i=1:trackerNum
     targetNumber=trackers(i,dim+2);
     target=getAgentById(targets,targetNumber);
+    while target==0
+        targetNumber=mod(targetNumber,targetNum)+1;
+        target=getAgentById(targets,targetNumber);
+    end
     targetPos=target(1:dim);
     direction=targetPos-trackers(i,1:dim);
     dist=norm(direction);

7dbc63820bf14ecb7d2b3a9cd56017023712dc86
Kilgrobil <alexmarro@hotmail.fr>
2017-11-16 14:18:30 +0100

    Amelioration poster

--- plotPotential.m
+++ plotPotential.m
@@ -1,13 +1,21 @@
 %This plots the potential from the point of view of a Tracker. 
-%There is an other Tracker in [2.1,2.1].
-%There is a yakuza in [5.1,7.7].
+%There are other Trackers in [2.1,2.1],[3.3,3.9].
+%There are other Yakuzas in [5.1,7.7],[8.1,2.9].
 
 
 [X,Y] = meshgrid(0:0.2:10);
 Z = zeros(size(X));
 for i = 1:size(X)
     for j=1:size(X)
-        Z(i,j) =     end
+        Z(i,j) = 2/sqrt((X(i,j)-2.1)^2+(Y(i,j)-2.1)^2);
+        Z(i,j) = Z(i,j) + 2/sqrt((X(i,j)-3.3)^2+(Y(i,j)-3.9)^2);
+        Z(i,j) = Z(i,j) - 1/((X(i,j)-5.1)^2+(Y(i,j)-7.7)^2);
+        Z(i,j) = Z(i,j) - 3/sqrt((X(i,j)-5.1)^2+(Y(i,j)-7.7)^2);
+        Z(i,j) = Z(i,j) - 1/((X(i,j)-8.1)^2+(Y(i,j)-2.9)^2);
+        Z(i,j) = Z(i,j) - 3/sqrt((X(i,j)-8.1)^2+(Y(i,j)-2.9)^2);
+        Z(i,j) = Z(i,j) + 1/X(i,j)^2 + 1/(X(i,j)-10)^2;
+        Z(i,j) = Z(i,j) + 1/Y(i,j)^2 + 1/(Y(i,j)-10)^2; 
+    end
 end
 surf(X,Y,Z)
 set(gca,'color',[1 1 1])
